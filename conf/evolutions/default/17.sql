7d0ac1a492e11ec64442e2ea1e8868f70523a1fc
macorains <mac.rainshrine@gmail.com>
2018-08-13 10:49:39 +0900

    Evolutionが本番環境でエラーになるため修正

--- conf/evolutions/default/17.sql
+++ conf/evolutions/default/17.sql
@@ -1,7 +1,7 @@
 # --- !Ups
-ALTER TABLE `sform2s_store`.`d_transfer_tasks`
+ALTER TABLE D_TRANSFER_TASKS
 CHANGE COLUMN `FORM_ID` `FORM_ID` VARCHAR(128) GENERATED ALWAYS AS (json_unquote(json_extract(`CONFIG`,'$.formId'))) VIRTUAL ;
 
 # --- !Downs
-ALTER TABLE `sform2s_store`.`d_transfer_tasks`
+ALTER TABLE D_TRANSFER_TASKS
 CHANGE COLUMN `FORM_ID` `FORM_ID` INT GENERATED ALWAYS AS (json_unquote(json_extract(`CONFIG`,'$.formId'))) VIRTUAL ;
