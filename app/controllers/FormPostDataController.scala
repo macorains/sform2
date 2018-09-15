c25324cf7d71c46bc2a0c1d2c9f6bf17209e0bd2
macorains <mac.rainshrine@gmail.com>
2018-09-15 12:18:30 +0900

    FormPostDataController.getPostDataを仮実装

--- app/controllers/FormPostDataController.scala
+++ app/controllers/FormPostDataController.scala
@@ -2,11 +2,12 @@ package controllers
 
 import com.mohiva.play.silhouette.api.Silhouette
 import javax.inject.Inject
+
 import org.webjars.play.WebJarsUtil
 import play.api.Environment
 import play.api.db.DBApi
 import play.api.i18n.I18nSupport
-import play.api.libs.json.Json
+import play.api.libs.json.{ JsValue, Json, Reads, Writes }
 import play.api.mvc.{ AbstractController, ControllerComponents }
 import utils.auth.DefaultEnv
 
@@ -22,8 +23,16 @@ class FormPostDataController @Inject() (
   ex: ExecutionContext
 ) extends AbstractController(components) with I18nSupport {
 
+  case class postDataResponse(total: String, page: String, records: String, rows: JsValue)
+  object postDataResponse {
+    implicit def jsonPostDataResponseWrites: Writes[postDataResponse] = Json.writes[postDataResponse]
+    implicit def jsonPostDataResponseReads: Reads[postDataResponse] = Json.reads[postDataResponse]
+  }
+
   def getPostData() = silhouette.UnsecuredAction.async { implicit request =>
-    Future.successful(Ok(Json.obj("status" -> "OK", "message" -> ("Hello"))).as("application/json; charset=UTF-8"))
+    val res = postDataResponse("100", "1", "1", Json.toJson("aaa"))
+
+    Future.successful(Ok(Json.toJson(res)).as("application/json; charset=UTF-8"))
   }
 
 }
