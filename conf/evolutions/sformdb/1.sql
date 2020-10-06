d4abe30f7240f2c395a7b7779a4453ecddbdbdfb
macorains <mac.rainshrine@gmail.com>
2020-10-06 23:04:13 +0900

    SFORM-41 evolutions

---
+++ conf/evolutions/sformdb/1.sql
@@ -0,0 +1,16 @@
+# --- !Ups
+
+CREATE TABLE `M_USER` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `UID` varchar(45) NOT NULL,
+  `NAME` varchar(45) DEFAULT NULL,
+  `EMAIL` varchar(100) NOT NULL,
+  `STATUS` int(11) NOT NULL,
+  `PASSWORD` varchar(128) DEFAULT NULL,
+  `CREATED` datetime DEFAULT NULL,
+  `MODIFIED` datetime DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+
+# --- !Downs
+  drop table M_USER;
