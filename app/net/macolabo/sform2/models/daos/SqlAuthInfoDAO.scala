139c5d54d7a80caf6b634e2e3986dd9d2ffd30dc
macorains <mac.rainshrine@gmail.com>
2021-09-18 23:55:25 +0900

    SFORM-52 テストコードを書ける形に変更

--- app/net/macolabo/sform2/models/daos/SqlAuthInfoDAO.scala
+++ app/net/macolabo/sform2/models/daos/SqlAuthInfoDAO.scala
@@ -9,88 +9,116 @@ import scalikejdbc._
 
 import scala.reflect.ClassTag
 
+import scala.concurrent.ExecutionContext.Implicits.global
+
 /**
- * パスワード情報用DAO
+ * 認証情報用DAO
+ * ※このDAOはSilhouetteのクラスをオーバーライドする必要がある関係で、トランザクション境界をDAO内にしている
  * @param ct classTag
  */
 class SqlAuthInfoDAO(implicit ct: ClassTag[PasswordInfo]) extends DelegableAuthInfoDAO[PasswordInfo] {
   val classTag: ClassTag[PasswordInfo] = ct
-  override def find(loginInfo: LoginInfo): Future[Option[PasswordInfo]] =
-    Future.successful(
-      DB localTx { implicit s =>
-        val f = AuthInfo.syntax("f")
-        withSQL (
-          select(
-            f.authinfo_id,
-            f.provider_id,
-            f.provider_key,
-            f.hasher,
-            f.password,
-            f.salt
-          )
-            .from(AuthInfo as f)
-            .where
-            .eq(f.provider_id, loginInfo.providerID)
-            .and
-            .eq(f.provider_key, loginInfo.providerKey)
-        ).map(rs => AuthInfo(rs)).single.apply.map(toPasswordInfo)
-      }
-    )
 
-  override def update(loginInfo: LoginInfo, passwordInfo: PasswordInfo): Future[PasswordInfo] =
-    Future.successful {
-      DB localTx { implicit s =>
-        withSQL {
-          val c = AuthInfo.column
-          QueryDSL.update(AuthInfo).set(
-            c.hasher -> passwordInfo.hasher,
-            c.password -> passwordInfo.password,
-            c.salt -> passwordInfo.salt
-          ).where
-            .eq(c.provider_id, loginInfo.providerID)
-            .and
-            .eq(c.provider_key, loginInfo.providerKey)
-        }.update().apply()
-        passwordInfo
-      }
+  override def find(loginInfo: LoginInfo): Future[Option[PasswordInfo]] = {
+      DB.localTx(implicit session => {
+        _find(loginInfo)
+      })
     }
 
-  override def remove(loginInfo: LoginInfo): Future[Unit] =
+  override def update(loginInfo: LoginInfo, passwordInfo: PasswordInfo): Future[PasswordInfo] = {
+    DB.localTx(implicit session => {
+      _update(loginInfo, passwordInfo)
+    })
+  }
+
+  override def remove(loginInfo: LoginInfo): Future[Unit] = {
+    DB.localTx(implicit session => {
+      _remove(loginInfo)
+    })
+  }
+
+  override def save(loginInfo: LoginInfo, passwordInfo: PasswordInfo): Future[PasswordInfo] = {
+    DB.localTx(implicit session => {
+      _save(loginInfo, passwordInfo)
+    })
+  }
+
+  override def add(loginInfo: LoginInfo, passwordInfo: PasswordInfo): Future[PasswordInfo] = {
+    DB.localTx(implicit session => {
+      _add(loginInfo, passwordInfo)
+    })
+  }
+
+  def _find(loginInfo: LoginInfo)(implicit session: DBSession): Future[Option[PasswordInfo]] = {
     Future.successful {
-      DB localTx { implicit s =>
-        withSQL {
-          val c = AuthInfo.column
-          QueryDSL.delete
-            .from(AuthInfo)
-            .where
-            .eq(c.provider_id, loginInfo.providerID)
-            .and
-            .eq(c.provider_key, loginInfo.providerKey)
-        }.update.apply
-      }
+      val f = AuthInfo.syntax("f")
+      withSQL(
+        select(
+          f.authinfo_id,
+          f.provider_id,
+          f.provider_key,
+          f.hasher,
+          f.password,
+          f.salt
+        )
+          .from(AuthInfo as f)
+          .where
+          .eq(f.provider_id, loginInfo.providerID)
+          .and
+          .eq(f.provider_key, loginInfo.providerKey)
+      ).map(rs => AuthInfo(rs)).single().apply().map(toPasswordInfo)
+    }
+  }
+
+  def _remove(loginInfo: LoginInfo)(implicit session:DBSession): Future[Unit] =
+    Future.successful {
+      withSQL {
+        val c = AuthInfo.column
+        QueryDSL.delete
+          .from(AuthInfo)
+          .where
+          .eq(c.provider_id, loginInfo.providerID)
+          .and
+          .eq(c.provider_key, loginInfo.providerKey)
+      }.update().apply()
     }
 
-  override def save(loginInfo: LoginInfo, passwordInfo: PasswordInfo): Future[PasswordInfo] =
-    find(loginInfo) match {
-      case _: PasswordInfo => update(loginInfo, passwordInfo)
-      case _ => add(loginInfo, passwordInfo)
+  def _update(loginInfo: LoginInfo, passwordInfo: PasswordInfo)(implicit session: DBSession): Future[PasswordInfo] = {
+    Future.successful {
+      withSQL {
+        val c = AuthInfo.column
+        QueryDSL.update(AuthInfo).set(
+          c.hasher -> passwordInfo.hasher,
+          c.password -> passwordInfo.password,
+          c.salt -> passwordInfo.salt
+        ).where
+          .eq(c.provider_id, loginInfo.providerID)
+          .and
+          .eq(c.provider_key, loginInfo.providerKey)
+      }.update().apply()
+      passwordInfo
+    }
+  }
+
+  def _save(loginInfo: LoginInfo, passwordInfo: PasswordInfo)(implicit session: DBSession): Future[PasswordInfo] =
+    _find(loginInfo).flatMap {
+      case Some(s: PasswordInfo) => _update(loginInfo, passwordInfo)
+      case _ => _add(loginInfo, passwordInfo)
     }
 
-  override def add(loginInfo: LoginInfo, passwordInfo: PasswordInfo): Future[PasswordInfo] =
+  def _add(loginInfo: LoginInfo, passwordInfo: PasswordInfo)(implicit session: DBSession): Future[PasswordInfo] =
     Future.successful {
-      DB localTx { implicit s =>
-        withSQL {
-          val c = AuthInfo.column
-          insertInto(AuthInfo).namedValues(
-            c.provider_id -> loginInfo.providerID,
-            c.provider_key -> loginInfo.providerKey,
-            c.hasher -> passwordInfo.hasher,
-            c.password -> passwordInfo.password,
-            c.salt -> passwordInfo.salt
-          )
-        }.update.apply
-        passwordInfo
-      }
+      withSQL {
+        val c = AuthInfo.column
+        insertInto(AuthInfo).namedValues(
+          c.provider_id -> loginInfo.providerID,
+          c.provider_key -> loginInfo.providerKey,
+          c.hasher -> passwordInfo.hasher,
+          c.password -> passwordInfo.password,
+          c.salt -> passwordInfo.salt
+        )
+      }.update().apply()
+      passwordInfo
     }
 
   private def toPasswordInfo(authInfo: AuthInfo) = {
