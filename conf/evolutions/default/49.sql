9e8281b190b2d268ee96dbc40da60f7b0fd738ff
macorains <mac.rainshrine@gmail.com>
2022-08-09 23:07:42 +0900

    SFORM-66 SQL追加

--- conf/evolutions/default/49.sql
+++ conf/evolutions/default/49.sql
@@ -11,6 +11,9 @@ alter table d_form_transfer_task_mail
 alter table d_form_transfer_task_mail
     add cc_address_field varchar(255) null after cc_address_id;
 
+alter table d_form_transfer_task_mail
+    modify to_address varchar(255) null;
+
 # --- !Downs
 alter table `d_form_transfer_task_mail`
 drop column `to_address_id`;
@@ -23,3 +26,6 @@ drop column `cc_address_id`;
 
 alter table `d_form_transfer_task_mail`
 drop column `cc_address_field`;
+
+alter table d_form_transfer_task_mail
+    modify to_address varchar(255) not null;
