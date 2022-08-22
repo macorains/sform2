3fefdfb9be5866167e64286776d5f6fdd18d35f0
macorains <mac.rainshrine@gmail.com>
2022-08-23 00:12:44 +0900

    SFORM-50 CryptoクラスをDomain用に書き換え

--- domain/src/main/scala/net/macolabo/sform2/domain/utils/Crypto.scala
+++ domain/src/main/scala/net/macolabo/sform2/domain/utils/Crypto.scala
@@ -7,23 +7,25 @@ import javax.crypto.Cipher
 import javax.crypto.spec.{IvParameterSpec, SecretKeySpec}
 import play.api.Configuration
 
-case class Crypto @Inject()(config: Configuration){
-
-  private val Algorithm = config.get[String]("sform.crypt.algorithm.cipher")
-  private val Key = new SecretKeySpec(Base64.getDecoder.decode(config.get[String]("sform.crypt.key")), config.get[String]("sform.crypt.algorithm.key"))
-  private val IvSpec = new IvParameterSpec(new Array[Byte](16))
-  private val Charset = config.get[String]("sform.crypt.charset")
+case class Crypto (
+  ivs: Array[Byte],
+  secretKey: Array[Byte],
+  algorithmCipher: String,
+  algorithmKey: String,
+  charset: String
+){
+  private val Key = new SecretKeySpec(secretKey, algorithmKey)
+  private val IvSpec = new IvParameterSpec(ivs)
+  private val encryptCipher = Cipher.getInstance(algorithmCipher)
+  private val decryptCipher = Cipher.getInstance(algorithmCipher)
+  encryptCipher.init(Cipher.ENCRYPT_MODE, Key, IvSpec)
+  decryptCipher.init(Cipher.DECRYPT_MODE, Key, IvSpec)
 
   def encrypt(text: String): String = {
-    val cipher = Cipher.getInstance(Algorithm)
-    cipher.init(Cipher.ENCRYPT_MODE, Key, IvSpec)
-
-    new String(Base64.getEncoder.encode(cipher.doFinal(text.getBytes(Charset))), Charset)
+    new String(Base64.getEncoder.encode(encryptCipher.doFinal(text.getBytes(charset))), charset)
   }
 
   def decrypt(text: String): String = {
-    val cipher = Cipher.getInstance(Algorithm)
-    cipher.init(Cipher.DECRYPT_MODE, Key, IvSpec)
-    new String(cipher.doFinal(Base64.getDecoder.decode(text.getBytes(Charset))), Charset)
+    new String(decryptCipher.doFinal(Base64.getDecoder.decode(text.getBytes(charset))), charset)
   }
 }
