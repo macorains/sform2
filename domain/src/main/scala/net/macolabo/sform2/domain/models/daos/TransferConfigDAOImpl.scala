b5bb00cffc6ff3d4646693f51eba7cb86eec36ab
macorains <mac.rainshrine@gmail.com>
2023-11-18 23:17:21 +0900

    SFORM-95　データ0件時の不具合fix

--- domain/src/main/scala/net/macolabo/sform2/domain/models/daos/TransferConfigDAOImpl.scala
+++ domain/src/main/scala/net/macolabo/sform2/domain/models/daos/TransferConfigDAOImpl.scala
@@ -74,7 +74,7 @@ class TransferConfigDAOImpl extends TransferConfigDAO {
       val c = TransferConfig.column
       insert.into(TransferConfig).namedValues(
         c.type_code -> transferConfig.type_code,
-        c.config_index -> (getMaxIndex(transferConfig.user_group).getOrElse(0) + 1),
+        c.config_index -> (getMaxIndex(transferConfig.user_group) + 1),
         c.name -> transferConfig.name,
         c.status -> transferConfig.status,
         c.user_group -> transferConfig.user_group,
@@ -132,6 +132,6 @@ class TransferConfigDAOImpl extends TransferConfigDAO {
         .from(TransferConfig as s)
         .where
         .eq(s.user_group, userGroup)
-    ).map(_.int(1)).single().apply()
+    ).map(_.intOpt(1)).single().apply().flatten.getOrElse(0)
   }
 }
