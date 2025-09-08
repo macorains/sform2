80d287fed10f6196aec594c0410d4e24f2243309
macorains <mac.rainshrine@gmail.com>
2025-09-08 21:42:24 +0900

    SFORM-110 テスト調整

--- domain/src/test/scala/net/macolabo/sform2/domain/services/ApiToken/ApiTokenServiceImplSpec.scala
+++ domain/src/test/scala/net/macolabo/sform2/domain/services/ApiToken/ApiTokenServiceImplSpec.scala
@@ -1,24 +1,19 @@
 package net.macolabo.sform2.domain.services.ApiToken
 
-import net.macolabo.sform2.domain.models.daos.{ApiTokenDAO, ApiTokenDAOImpl}
+import net.macolabo.sform2.domain.models.daos.ApiTokenDAO
 import net.macolabo.sform2.domain.models.entity.api_token.ApiToken
 import net.macolabo.sform2.domain.models.helper.SformTestHelper
 import net.macolabo.sform2.domain.services.ApiToken.insert.ApiTokenInsertRequest
-import org.apache.pekko.actor.ActorSystem
-import org.apache.pekko.testkit.TestKit
 import org.mockito.ArgumentMatchers._
-import org.mockito.Mockito._
 import org.mockito.MockitoSugar
-import org.mockito.MockitoSugar.{doReturn, spy, when}
-import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.must.Matchers
-import org.scalatest.wordspec.AnyWordSpecLike
 import org.scalatestplus.play.PlaySpec
 import play.inject.guice.GuiceApplicationBuilder
 import play.api.inject.bind
 import scalikejdbc.DBSession
 
-import java.time.LocalDateTime
+import java.util.Base64
+
 
 class ApiTokenServiceImplSpec extends PlaySpec
   with SformTestHelper
@@ -37,8 +32,8 @@ class ApiTokenServiceImplSpec extends PlaySpec
       val service = app.injector.instanceOf(classOf[ApiTokenServiceImpl])
 
       val response = service.insert(ApiTokenInsertRequest(90L), "hoge", "fuga")
-      response.token.length mustBe 32
+      val decodedBytes = Base64.getUrlDecoder.decode(response.token)
+      decodedBytes.length mustBe 16
     }
   }
-
 }
