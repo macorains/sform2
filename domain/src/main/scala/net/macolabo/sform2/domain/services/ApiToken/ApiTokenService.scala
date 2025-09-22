cc0aabb57efb440646ce4c7c5c25c63fee5bed08
macorains <mac.rainshrine@gmail.com>
2025-09-22 19:21:40 +0900

    SFORM-115 ApiToken周りも同様の対応

--- domain/src/main/scala/net/macolabo/sform2/domain/services/ApiToken/ApiTokenService.scala
+++ domain/src/main/scala/net/macolabo/sform2/domain/services/ApiToken/ApiTokenService.scala
@@ -1,11 +1,12 @@
 package net.macolabo.sform2.domain.services.ApiToken
 
+import net.macolabo.sform2.domain.models.SessionInfo
 import net.macolabo.sform2.domain.services.ApiToken.insert.{ApiTokenInsertRequest, ApiTokenInsertResponse}
 import play.api.mvc.Session
 
 import java.time.LocalDateTime
 
 trait ApiTokenService {
-  def insert(apiTokenInsertRequest: ApiTokenInsertRequest, session: Session): ApiTokenInsertResponse
-  def getExpiry(session: Session): Option[LocalDateTime]
+  def insert(apiTokenInsertRequest: ApiTokenInsertRequest, sessionInfo: SessionInfo): ApiTokenInsertResponse
+  def getExpiry(sessionInfo: SessionInfo): Option[LocalDateTime]
 }
