eadfe3278551ab566a3ef0133fb20406063395bd
macorains <mac.rainshrine@gmail.com>
2025-10-28 21:57:46 +0900

    SFORM-123 間違えてコミットした箇所の戻し

--- adminApi/app/net/macolabo/sform2/controllers/UserController.scala
+++ adminApi/app/net/macolabo/sform2/controllers/UserController.scala
@@ -58,7 +58,6 @@ class UserController @Inject() (
             jsBody.validate[UserSaveRequest].fold(
               errors => BadRequest(JsError.toJson(errors)),
               value => {
-                userService.retrieveByEmail()
                 userService.save(value, sessionInfo)
                 Ok
               }
