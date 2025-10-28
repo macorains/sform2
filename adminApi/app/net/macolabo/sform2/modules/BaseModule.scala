e94d4b4ab41eda5353ca6676e331baff9e6fa79e
macorains <mac.rainshrine@gmail.com>
2025-10-28 21:55:38 +0900

    SFORM-123 不要モジュール削除

--- adminApi/app/net/macolabo/sform2/modules/BaseModule.scala
+++ adminApi/app/net/macolabo/sform2/modules/BaseModule.scala
@@ -4,7 +4,6 @@ import com.google.inject.AbstractModule
 import net.codingwell.scalaguice.ScalaModule
 import net.macolabo.sform2.domain.models.daos.{ApiTokenDAO, ApiTokenDAOImpl, AuthTokenDAO, AuthTokenDAOImpl, FormDAO, FormDAOImpl, PostdataDAO, PostdataDAOImpl, TransferConfigDAO, TransferConfigDAOImpl, TransferConfigMailAddressDAO, TransferConfigMailAddressDAOImpl, TransferConfigMailDAO, TransferConfigMailDAOImpl, TransferConfigSalesforceDAO, TransferConfigSalesforceDAOImpl, TransferConfigSalesforceObjectDAO, TransferConfigSalesforceObjectDAOImpl, TransferConfigSalesforceObjectFieldDAO, TransferConfigSalesforceObjectFieldDAOImpl, TransferDetailLogDAO, TransferDetailLogDAOImpl, TransferLogDAO, TransferLogDAOImpl, UserDAO, UserDAOImpl}
 import net.macolabo.sform2.domain.services.ApiToken.{ApiTokenService, ApiTokenServiceImpl}
-import net.macolabo.sform2.domain.services.AuthToken.{AuthTokenService, AuthTokenServiceImpl}
 import net.macolabo.sform2.domain.services.TransferConfig.{TransferConfigService, TransferConfigServiceImpl}
 import net.macolabo.sform2.domain.services.User.{UserService, UserServiceImpl}
 
@@ -33,7 +32,6 @@ class BaseModule extends AbstractModule with ScalaModule {
     bind[UserDAO].to[UserDAOImpl]
     // Services
     bind[ApiTokenService].to[ApiTokenServiceImpl]
-    bind[AuthTokenService].to[AuthTokenServiceImpl]
     bind[TransferConfigService].to[TransferConfigServiceImpl]
     bind[UserService].to[UserServiceImpl]
   }
